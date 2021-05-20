a27ea6b2661ddd3024781d3487c7b433d07f08f8
Doom1Nator <li14285757142@126.com>
2021-05-20 10:34:26 +0800

    Update imageConvert.js

--- server-side/tasks/imageConvert.js
+++ server-side/tasks/imageConvert.js
@@ -9,7 +9,7 @@ function convertImageToBase64(fileName) {
     try{
         const data = fs.readFileSync(`.\/tasks\/images\/${fileName}`, 'binary');
         const buffer = new Buffer.from(data, 'binary');
-        let imagebase64 = 'data: image/' + getImageType(fileName) + ';base64,' + buffer.toString('base64');
+        let imagebase64 = 'data:image/' + getImageType(fileName) + ';base64,' + buffer.toString('base64');
         return imagebase64;
     }catch(e){
         throw 'seed faild: open images resources failed. please make sure you start the server from ./server-side/ directory.'
